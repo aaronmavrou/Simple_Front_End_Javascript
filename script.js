e94a07a08eeb9465f98d4ef03e6ca57e2274cf03
aaronmavrou <35606894+aaronmavrou@users.noreply.github.com>
2018-10-21 18:03:09 -0400

    commit

--- script.js
+++ script.js
@@ -1,56 +1,132 @@
 
 
-function quantity(itemId, amountId, quantityId, priceId, taxId){
+function quantity(itemId, amountId, quantityId, priceId, taxId, buttonId) {
     var itemsLeft = document.getElementById(quantityId).innerHTML;
     var itemsPurchased = document.getElementById(amountId).value;
-    
-    if ((parseInt(itemsLeft)-parseInt(itemsPurchased)) < 0){
+
+    if ((parseInt(itemsLeft) - parseInt(itemsPurchased)) < 0) {
         alert("This is more than the available quantity. Please enter a new quantity in the range");
+    }
+    
+    else if (itemsPurchased <= 0){
+        alert("This is an invalid quantity number. Please try again");
     }
 
     else {
-        document.getElementById(quantityId).innerHTML = parseInt(itemsLeft)-parseInt(itemsPurchased);
+        document.getElementById(quantityId).innerHTML = parseInt(itemsLeft) - parseInt(itemsPurchased);
+        quantityIds.push(quantityId);
         updateCart(itemId, amountId, priceId, taxId);
+        document.getElementById(buttonId).disabled = true;
+        buttonIds.push(buttonId);
     }
 }
+
+buttonIds = [];
+quantityIds = [];
 inames = [];
 iqtyp = [];
 iprice = [];
 itax = [];
 
-function updateCart(itemId, amountId, priceId, taxId){
-        inames.push(document.getElementById(itemId).textContent);
-        iqtyp.push(document.getElementById(amountId).value);
-        iprice.push(parseFloat(document.getElementById(priceId).innerHTML));
-        itax.push(parseFloat(document.getElementById(taxId).innerHTML));
-        showCart();
+function updateCart(itemId, amountId, priceId, taxId) {
+    inames.push(document.getElementById(itemId).textContent);
+    iqtyp.push(document.getElementById(amountId).value);
+    iprice.push(parseFloat(document.getElementById(priceId).innerHTML));
+    itax.push(parseFloat(document.getElementById(taxId).innerHTML));
+    showCart();
 }
 
-function showCart(){
+function showCart() {
     cartData = '<table><tr><th>Product</th><th>Quantity</th><th>Price</th><th>Total</th></tr>';
     total = 0;
 
-    for(i=0; i<inames.length; i++){
-        total+= iqtyp[i]*iprice[i]*(1+itax[i]);
-        cartData += "<tr><td>" + inames[i] + "</td><td>" + iqtyp[i] + "</td><td>" + iprice[i] + "</td><td>"+ iqtyp[i]*iprice[i]*(1+itax[i]) + "</td><td><button onclick='deleteElement(" + i +")'>Delete</button></td></tr>"
+    for (i = 0; i < inames.length; i++) {
+        total += iqtyp[i] * iprice[i] * (1 + itax[i]);
+        cartData += "<tr><td>" + inames[i] + "</td><td>" + iqtyp[i] + "</td><td>" + iprice[i] + "</td><td>" + iqtyp[i] * iprice[i] * (1 + itax[i]) + "</td><td><button onclick='deleteElement(" + i + ")'>Delete</button></td>" + "</td><td><button onclick='increaseQuantity(" + i + ")'>+</button></td>" + "</td><td><button onclick='decreaseQuantity(" + i + ")'>-</button></td></tr>";
     }
     cartData += '<tr><td></td><td></td><td><t/d><td>' + total + '</td></tr></table>';
 
     document.getElementById('basket').innerHTML = cartData;
 }
 
-function deleteElement(x){
-    inames.splice(x,1);
-    iqtyp.splice (x,1);
-    iprice.splice(x,1);
+function increaseQuantity(a) {
+    if (parseInt(document.getElementById(quantityIds[a]).innerHTML) > 0) {
+        iqtyp[a] = parseInt(iqtyp[a]) + 1;
+        document.getElementById(quantityIds[a]).innerHTML = parseInt(document.getElementById(quantityIds[a]).innerHTML) - 1;
+    }
+
+    else {
+        alert("This is more than the available quantity. Please enter a new quantity in the range");
+    }
     showCart();
 }
 
+function decreaseQuantity(a) {
+    iqtyp[a] = parseInt(iqtyp[a]) - 1;
+    document.getElementById(quantityIds[a]).innerHTML = parseInt(document.getElementById(quantityIds[a]).innerHTML) + 1;
+    if (parseInt(iqtyp[a]) == 0) {
+        inames.splice(a, 1);
+        iqtyp.splice(a, 1);
+        iprice.splice(a, 1);
+        itax.splice(a, 1);
+        quantityIds.splice(a, 1);
+        document.getElementById(buttonIds[a]).disabled = false;
+        buttonIds.splice(a,1);
+    }
+    showCart();
+}
+
+function deleteElement(x) {
+    document.getElementById(quantityIds[x]).innerHTML = parseInt(iqtyp[x]) + parseInt(document.getElementById(quantityIds[x]).innerHTML);
+    inames.splice(x, 1);
+    iqtyp.splice(x, 1);
+    iprice.splice(x, 1);
+    itax.splice(x, 1);
+    quantityIds.splice(x, 1);
+    document.getElementById(buttonIds[x]).disabled = false;
+    buttonIds.splice(x,1);
+    showCart();
+}
+
+function clearBasket() {
+    var torf = confirm('Are you sure?');
+    if (torf) {
+        for (i = 0; i < inames.length; i) {
+            var itemTotal = parseInt(document.getElementById(quantityIds[i]).innerHTML);
+            document.getElementById(quantityIds[i]).innerHTML = parseInt(iqtyp[i]) + itemTotal;
+            inames.splice(0, 1);
+            iqtyp.splice(0, 1);
+            iprice.splice(0, 1);
+            itax.splice(0, 1);
+            quantityIds.splice(0, 1);
+            document.getElementById(buttonIds[i]).disabled = false;
+            buttonIds.splice(0,1);
+        }
+        showCart();
+    }
+}
+
+function purchaseMade() {
+    var torf = confirm('Are you sure?');
+
+    if (torf) {
+        for (i = 0; i < inames.length; i) {
+            inames.splice(0,1);
+            iqtyp.splice(0,1);
+            iprice.splice(0,1);
+            itax.splice(0,1);
+            quantityIds.splice(0,1);
+            document.getElementById(buttonIds[i]).disabled = false;
+            buttonIds.splice(0,1);
+        }
+        showCart();
+    }
+}
 
 
-function changeLanguage(){
+function changeLanguage() {
     var language = document.getElementsByName("language");
-    if(language[0].checked){
+    if (language[0].checked) {
         document.getElementById("Stick").innerHTML = "Stick";
         document.getElementById("Bat").innerHTML = "Bat";
         document.getElementById("Football").innerHTML = "Football";
