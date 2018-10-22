45839c55ada153df2c66aab8d868107c4da7fec2
aaronmavrou <35606894+aaronmavrou@users.noreply.github.com>
2018-10-21 23:13:51 -0400

    edits and modifications

--- script.js
+++ script.js
@@ -1,25 +1,61 @@
 
-taxIds = ['stickTax', 'batTax', 'footballTax' , 'gloveTax', 'shoeTax', 'baseballTax', 'puckTax', 'basketballTax', 'racketTax', 'gatoradeTax']
+taxIds = ['stickTax', 'batTax', 'footballTax', 'gloveTax', 'shoeTax', 'baseballTax', 'puckTax', 'basketballTax', 'racketTax', 'gatoradeTax']
 qIds = ['sticksLeft', 'batsLeft', 'footballsLeft', 'glovesLeft', 'shoesLeft', 'baseballsLeft', 'pucksLeft', 'basketballsLeft', 'racketsLeft', 'gatoradesLeft']
-itemIds = ['stick', 'bat', 'football', 'glove', 'shoes', 'baseball', 'puck', 'basketball','racket', 'gatorade']
+itemIds = ['stick', 'bat', 'football', 'glove', 'shoes', 'baseball', 'puck', 'basketball', 'racket', 'gatorade']
 
-function passwordStuff(){
-
-    
-}
-
-
-function adminEntersQuantity(newQuantityID, actualId ){
-    document.getElementById(actualId).innerHTML = document.getElementById(newQuantityID).value;
-}
-
-function password(passwordId){
-    var jacob = document.getElementById(passwordId).value;
-    if(jacob.toUpperCase() == "123"){
-        alert("it works hoe");
+function password(passwordId) {
+    var thePassword = document.getElementById(passwordId).value;
+    if (thePassword.toUpperCase() == "123") {
+        alert("You are logged in!");
         passwordStuff();
     }
 }
+
+function passwordStuff() {
+    for (i = 0; i < itemIds.length; i++) {
+        var btn = document.createElement("BUTTON");
+        var t = document.createTextNode("Change Amount of Stock  ");       // Create a text node
+        btn.appendChild(t);                                // Append the text to <button>
+        var addQuantity = document.createElement("input");
+        addQuantity.type = 'number';
+        addQuantity.value = 1;
+        addQuantity.size = 5;
+        addQuantity.id = i;
+        btn.setAttribute("onClick", "javascript: adminQuantity(" + i + ',' + addQuantity.id + ");");
+        document.getElementById(itemIds[i]).appendChild(btn);
+        document.getElementById(itemIds[i]).appendChild(addQuantity);
+
+        var addBreak = document.createElement("br");
+        document.getElementById(itemIds[i]).appendChild(addBreak);
+        var addBreak1 = document.createElement("br");
+        document.getElementById(itemIds[i]).appendChild(addBreak1);
+
+        var button = document.createElement("BUTTON");
+        var n = document.createTextNode("Change Tax Rate  ");       // Create a text node
+        button.appendChild(n);                                // Append the text to <button>
+        var theTax = document.createElement("input");
+        theTax.type = 'text';
+        theTax.value = 1;
+        theTax.size = 5;
+        theTax.id = i + 100;
+        button.setAttribute("onClick", "javascript: adminTax(" + i + ',' + theTax.id + ");");
+        document.getElementById(itemIds[i]).appendChild(button);
+        document.getElementById(itemIds[i]).appendChild(theTax);
+    }
+
+}
+
+function adminQuantity(identifier, amount) {
+    document.getElementById(qIds[identifier]).innerHTML = parseInt(document.getElementById(amount).value);
+
+}
+function adminTax(num, number) {
+    document.getElementById(taxIds[num]).innerHTML = parseFloat(document.getElementById(number).value);
+}
+
+
+
+
 
 function quantity(itemId, amountId, quantityId, priceId, taxId, buttonId) {
     var itemsLeft = document.getElementById(quantityId).innerHTML;
@@ -28,8 +64,8 @@ function quantity(itemId, amountId, quantityId, priceId, taxId, buttonId) {
     if ((parseInt(itemsLeft) - parseInt(itemsPurchased)) < 0) {
         alert("This is more than the available quantity. Please enter a new quantity in the range");
     }
-    
-    else if (itemsPurchased <= 0){
+
+    else if (itemsPurchased <= 0) {
         alert("This is an invalid quantity number. Please try again");
     }
 
@@ -63,9 +99,9 @@ function showCart() {
 
     for (i = 0; i < inames.length; i++) {
         total += iqtyp[i] * iprice[i] * (1 + itax[i]);
-        cartData += "<tr><td>" + inames[i] + "</td><td>" + iqtyp[i] + "</td><td>" + iprice[i] + "</td><td>" + iprice[i]*iqtyp[i] + "</td><td>" + itax[i] + "</td><td>" + iqtyp[i] * iprice[i] * (1 + itax[i]) + "</td><td><button onclick='deleteElement(" + i + ")'>Delete</button></td>" + "</td><td><button onclick='increaseQuantity(" + i + ")'>+</button></td>" + "</td><td><button onclick='decreaseQuantity(" + i + ")'>-</button></td></tr>";
+        cartData += "<tr><td>" + inames[i] + "</td><td>" + iqtyp[i] + "</td><td>" + iprice[i].toFixed(2) + "</td><td>" + (iprice[i] * iqtyp[i]).toFixed(2) + "</td><td>" + itax[i].toFixed(2) + "</td><td>" + (iqtyp[i] * iprice[i] * (1 + itax[i])).toFixed(2) + "</td><td><button onclick='deleteElement(" + i + ")'>Delete</button></td>" + "</td><td><button onclick='increaseQuantity(" + i + ")'>+</button></td>" + "</td><td><button onclick='decreaseQuantity(" + i + ")'>-</button></td></tr>";
     }
-    cartData += '<tr><td></td><td></td><td></td><td></td><td><t/d><td>' + total + '</td></tr></table>';
+    cartData += '<tr><td></td><td></td><td></td><td></td><td><t/d><td>' + total.toFixed(2) + '</td></tr></table>';
 
     document.getElementById('basket').innerHTML = cartData;
 }
@@ -92,7 +128,7 @@ function decreaseQuantity(a) {
         itax.splice(a, 1);
         quantityIds.splice(a, 1);
         document.getElementById(buttonIds[a]).disabled = false;
-        buttonIds.splice(a,1);
+        buttonIds.splice(a, 1);
     }
     showCart();
 }
@@ -105,7 +141,7 @@ function deleteElement(x) {
     itax.splice(x, 1);
     quantityIds.splice(x, 1);
     document.getElementById(buttonIds[x]).disabled = false;
-    buttonIds.splice(x,1);
+    buttonIds.splice(x, 1);
     showCart();
 }
 
@@ -121,7 +157,7 @@ function clearBasket() {
             itax.splice(0, 1);
             quantityIds.splice(0, 1);
             document.getElementById(buttonIds[i]).disabled = false;
-            buttonIds.splice(0,1);
+            buttonIds.splice(0, 1);
         }
         showCart();
     }
@@ -132,13 +168,13 @@ function purchaseMade() {
 
     if (torf) {
         for (i = 0; i < inames.length; i) {
-            inames.splice(0,1);
-            iqtyp.splice(0,1);
-            iprice.splice(0,1);
-            itax.splice(0,1);
-            quantityIds.splice(0,1);
+            inames.splice(0, 1);
+            iqtyp.splice(0, 1);
+            iprice.splice(0, 1);
+            itax.splice(0, 1);
+            quantityIds.splice(0, 1);
             document.getElementById(buttonIds[i]).disabled = false;
-            buttonIds.splice(0,1);
+            buttonIds.splice(0, 1);
         }
         showCart();
     }
